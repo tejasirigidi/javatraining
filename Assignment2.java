2b0b0b4e70adf67a89a86e7ce350c0d6024240d1
teja <teja.sirigidi@gmail.com>
2020-12-25 12:58:24 +0530

    Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,15 @@
+class Assignment2
+{
+	protected int a=10;
+	static int b=20;
+	public static void main(String args[])
+	{
+		int c=50;
+		int b=40;
+		Assignment2 as= new Assignment2();
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(as.b);
+		System.out.println(as.a);
+	}
+}
